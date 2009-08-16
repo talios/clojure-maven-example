d6981aaf2d59de03c8557c0fd1690aff192d72bb
Mark Derricutt <mark@talios.com>
2009-08-16 19:35:57 +1200

    Added an automated compojure test

--- src/test/clojure/test.clj
+++ src/test/clojure/test.clj
@@ -1,8 +1,25 @@
 (ns test
-    (:use [clojure.test :only [run-tests deftest is]]))
+    (:use [clojure.test :only [run-tests deftest is]])
+    (:use compojure)
+    (:use webapp)
+    (:require [clojure.http.resourcefully :as resourcefully]))
 
 ; Very simple test
 (deftest first-test []
     (is (= "this" "this")))
 
+
+; Add some automated tests for our web app
+; Start by declaring and starting the server
+
+(def *JETTY* (jetty-server {:port 8080} "/*" (servlet example-app)))
+(.start *JETTY*)
+
+(deftest compojure-test []
+  (let [response (resourcefully/get "http://localhost:8080/")]
+    (is (= (:code response) 200))))
+
 (run-tests)
+
+; And stop the server...
+(.stop *JETTY*)
